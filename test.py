88be2eafef6b380284be52c6cfda43df92c49114
Pankaj4152 <pankajgoyal4152@gmail.com>
2026-03-07 23:11:51 +0530

    req file

--- test.py
+++ test.py
@@ -22,24 +22,44 @@
 
 
 
-# from voice.asr import WhisperASR
+from voice.asr import WhisperASR
 # from intent.classifier import IntentClassifier
 # from actions.windows_executor import WindowsExecutor
-# from telemetry.logger import TelemetryLogger
+from telemetry.logger import TelemetryLogger
 
 
-# def main():
-#     logger = TelemetryLogger()
+def main():
+    logger = TelemetryLogger()
 
-#     asr = WhisperASR(logger=logger)
+    asr = WhisperASR(logger=logger)
 
-#     print("Press Ctrl+C to exit.\n")
+    print("Press Ctrl+C to exit.\n")
 
-#     while True:
-#         input("Press ENTER to record...")
-#         text = asr.transcribe()
-#         print("Returned text:", text)
+    while True:
+        input("Press ENTER to record...")
+        text = asr.transcribe()
+        print("Returned text:", text)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
+
+
+
+
+# from sarvamai import SarvamAI
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# client = SarvamAI(
+#     api_subscription_key=os.getenv("SARVAM_API_KEY"),
+# )
+
+# response = client.text.translate(
+#     input="Hi, My Name is Vinayak.",
+#     source_language_code="auto",
+#     target_language_code="gu-IN",
+#     speaker_gender="Male"
+# )
+
+# print(response)
